e15dc6b2141f634af9a59982ce6507093c2bd3eb
Sahil-Malik-08 <sahilabhibro11@gmail.com>
2026-04-17 11:58:13 +0530

    Update login API endpoint to production URL

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -22,7 +22,7 @@ const Login = ({ login }) => {
     setLoading(true);
 
     try {
-      const response = await fetch('http://localhost:5000/api/auth/login', {
+      const response = await fetch('https://swapskill-2-1p6q.onrender.com/api/auth/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
